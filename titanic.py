cf2ce7402eb4e965534e5d8c667c4f2be25e5f46
nedwards3 <nedwards3@ucmerced.edu>
2020-04-02 21:32:03 -0700

    Getting the pip imports
    There willl be more imports later though They will be from files.

---
+++ titanic.py
@@ -0,0 +1,15 @@
+import sys
+##should be default
+
+import numpy as np
+#pip install numpy
+import pandas as pd
+#pip install pandas
+import seaborn as sns
+#pip install seaborn
+import matplotlib.pyplot as pltzr
+#pip install matplotlib
+print(sys.version)
+##some of the imports requre python 3
+
+print("I have all the imports")
